3520d8be9a5d437039d84e6cfa1a5285241f0eba
berntjoh <berntj@viken.no>
2024-04-23 16:33:55 +0200

    FKS-701: add check for empty StartDato in GyldigehetsperiodeServce.isValid

--- src/main/java/no/fintlabs/role/GyldighetsperiodeService.java
+++ src/main/java/no/fintlabs/role/GyldighetsperiodeService.java
@@ -10,10 +10,15 @@ public class GyldighetsperiodeService {
 
     public static class NullPeriodeException extends RuntimeException {
     }
+    public static class NullPeriodeStartDatoException extends RuntimeException {
+    }
 
     public boolean isValid(Periode gyldighetsperiode, Date currentTime) {
         if (gyldighetsperiode == null) {
             throw new NullPeriodeException();
+        }
+        if (gyldighetsperiode.getStart() == null) {
+            throw new NullPeriodeStartDatoException();
         }
         return currentTime.after(gyldighetsperiode.getStart())
                 && isEndValid(gyldighetsperiode.getSlutt(), currentTime);
